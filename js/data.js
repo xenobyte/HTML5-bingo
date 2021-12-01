aecac51420467a0069ac019e07862d7d8a45dccd
xenobyte <timo@teebytes.net>
2021-12-01 16:00:09 +0100

    Modify for cyber bullshit talk

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
-var headerText = "Stuff Republicans Say";
+var headerText = "Bullsh*t Bingo";
 
-var footerText = "<a href='https://github.com/michaellevesque/HTML5-bingo' target='_blank'>Code available on github</a>";
+var footerText = "<a href='https://github.com/xenobyte/HTML5-bingo' target='_blank'>Code available on github</a>";
 
 var winText = "Winner";
 
@@ -9,35 +9,35 @@ var clickSnd = new Audio("audio/click.mp3");
 var winSnd = new Audio("audio/win.mp3");
 
 var JSONBingo = {"squares": [
-        {"square": "Only Atheists can get AIDS."},
-        {"square": "There would be less rape if all women were homemakers."},
-        {"square": "You can't get pregnant from Christmas rape."},
-        {"square": "Widespread contraception use leads to increased incidence of rape."},
-        {"square": "If you read 50 Shades of Grey, your going to hell."},
-        {"square": "Dads who conceive children through rape should have full visitation rights."},
-        {"square": "Homosexuality is a choice."},
-        {"square": "Everyone would be tempted to be gay if we didn't shame homosexuality."},
-        {"square": "There was not rape before feminism."},
-        {"square": "Marriage cures AIDS."},
-        {"square": "Church Pedophilia scandals result from tolerance of homosexuals."},
-        {"square": "Girls who watch Girls will never be happy wives."},
-        {"square": "Women subconsciously want to be raped; they send chemical signals that trigger men."},
-        {"square": "If we teach kids to put condoms on bananas, we might as well teach them how to use stripper poles."},
-        {"square": "If a girl gets drunk at a party, well, you know, I'm just sayin'."},
-        {"square": "This just shows that FemiNazi agenda is false."},
-        {"square": "Want to stop date rape? Stop Dating."},
-        {"square": "You can't take back the night; you never had it in the first place."},
-        {"square": "Middle school health class turn girls into prostitutes."},
-        {"square": "The solution to rape in the military is to kick out the women."},
-        {"square": "You want to know that your fellow soldier's got you back, not that he's staring at it."},
-        {"square": "Feminism's secret goal is to see all Christian boys aborted."},
-        {"square": "Well she has had many prior sexual partners you know."},
-        {"square": "If we're going to teach kids safe sex, we might as well just inject them with AIDS."},            
-        {"square": "If we stopped all imports the US economy would be stronger"},    	
-        {"square": "Immigration is the problem, cheap labor corrupts."},    	
-        {"square": "China has spies in all US companies, stealing our ideas."}, 
-        {"square": "If your CEO is not republican he is a net drain on the economy."},    	
-        {"square": "Farm subsides are beneficial because they help the economy."},    	
-        {"square": "If Marijuana was legal all our kids would use it."} 	
+        {"square": "Exploit"},
+        {"square": "Social Engineering"},
+        {"square": "Angriffsvektoren"},
+        {"square": "Malware"},
+        {"square": "Ransomware"},
+        {"square": "Social Engineering"},
+        {"square": "Reverse Shell"},
+        {"square": "Maschine-In-The-Middle"},
+        {"square": "Firewall"},
+        {"square": "Policy"},
+        {"square": "Authentifizierung"},
+        {"square": "Passwort Manager"},
+        {"square": "Scriptkiddies"},
+        {"square": "Advanced Persistent Threat"},
+        {"square": "Irgendwas mit Cyber"},
+        {"square": "Portscan"},
+        {"square": "Payload"},
+        {"square": "Trojaner"},
+        {"square": "Software-Defined-Radio"},
+        {"square": "Evil Twin Attack"},
+        {"square": "Internet of Things"},
+        {"square": "Office Makro"},
+        {"square": "Frontend"},
+        {"square": "Backend"},            
+        {"square": "Cookie"},    	
+        {"square": "HTTP"},    	
+        {"square": "Hacking"}, 
+        {"square": "Confidentiality"},    	
+        {"square": "Integrity"},    	
+        {"square": "Availability"} 	
     ]
 };
